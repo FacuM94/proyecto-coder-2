77a514c2696e65430eae299ecc88a731f8b30244
Facundo Montalban <facundomontalban@gmail.com>
2022-10-07 17:25:34 -0300

    agrego templates

--- proyectocoder/appcoder/views.py
+++ proyectocoder/appcoder/views.py
@@ -7,3 +7,20 @@ from django.http import HttpResponse
 def mostrar_inicio(request):
 
     return HttpResponse("Hola mundo")
+
+
+def mostrar_inicio(request):
+    return render(request, "appcoder/inicio.html")
+
+def mostrar_cursos(request):
+    return render(request, "appcoder/cursos.html")
+
+def mostrar_entregables(request):
+    return render(request, "appcoder/mostrar_entregables.html")
+
+def mostrar_estudiantes(request):
+    return render(request, "appcoder/mostrar_estudiantes.html")
+
+def mostrar_profesores(request):
+    return render(request, "appcoder/mostrar_profesores.html")
+
